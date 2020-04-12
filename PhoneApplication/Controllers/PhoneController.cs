37baf1900ae86d6140c3d414b07329541e45a0b1
Mohakkapoor98 <200425207@student.georgianc.on.ca>
2020-04-11 21:43:11 -0400

    Lab 3 completed

---
+++ PhoneApplication/Controllers/PhoneController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PhoneApplication.Models;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            var PhoneNew = new List<Phone>
+              {
+                  new Phone {PhoneName = "Iphone 11 Pro Max", ScreenSize = 155, Manufacturer = "Apple" , DateReleased = new DateTime(2019,10,1), MSRP = 2000 },
+                  new Phone {PhoneName = "F9", ScreenSize = 175, Manufacturer = "Oppo" , DateReleased = new DateTime(2018,5,1), MSRP = 1000 },
+                  new Phone {PhoneName = "Smartpad", ScreenSize = 100, Manufacturer = "Nokia" , DateReleased = new DateTime(2016,4,7), MSRP = 2000 },
+              };
+            return View(PhoneNew);
+        }
+    }
+}
---
+++ PhoneApplication/Models/Phone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+            PhoneName = "";
+            Manufacturer = "";
+            DateReleased = new DateTime();
+
+        }
+
+        public int PhoneID { get; set; }
+
+        public String PhoneName { get; set; }
+
+        public String Manufacturer { get; set; }
+
+        public double MSRP { get; set; }
+
+        public double ScreenSize { get; set; }
+
+        public DateTime DateReleased { get; set; }
+
+
+
+    }
+}
